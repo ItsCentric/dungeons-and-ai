14cbb32ca71c32db8724fde28683b9769bcc80c8
Isaiah <isaiahmstockton@gmail.com>
2024-09-25 19:55:13 -0400

    feat: add shadcn

--- src/routes/auth/signin/+page.svelte
+++ src/routes/auth/signin/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { PUBLIC_HCAPTCHA_SITEKEY } from '$env/static/public';
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	onMount(() => {
 		if (typeof window !== 'undefined') {
@@ -21,9 +22,7 @@
 <main>
 	<h1 class="text-2xl font-bold">Sign in</h1>
 	<form method="post">
-		<div id="hcaptcha-container"></div>
-		<button formaction="?/anon" class="rounded-lg bg-green-600 px-2 py-1 text-white"
-			>Anonymous Sign in</button
-		>
+		<div id="hcaptcha-container" data-theme="dark"></div>
+		<Button type="submit" formaction="?/anon">Anonymous Sign in</Button>
 	</form>
 </main>
